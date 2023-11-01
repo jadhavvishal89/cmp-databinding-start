f170b166a0ad56e901bba840baf466452cddea2f
priya <awatepriya@gmail.com>
2023-11-01 23:08:57 +0530

    Passdata from Child to Parent with Output and evet emiter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,21 @@ export class AppComponent {
   serverElements = [{type:'server',name:'TestServer', content:'Just a test!'}];
 
   
+  onServerAdded(serverData:{serverName:string,serverContent:string}) {
+    console.log(serverData.serverName);
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content:serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content:blueprintData.serverContent
+    });
+  }
  
 }
